1679b33022ea521cbfa67ff340a7128bc4f37023
Cook1619 <cook6348@gmail.com>
2019-07-31 21:23:16 -0500

    Using findIndex explained in an array of objects

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,6 +1,19 @@
 //Arrays are a method heavy data type with lots of different helpful methods to help us along the way
 
-const notes = ['Note 1', 'Note 2', 'Note 3'];
+const notes = [
+    {
+        title: 'My next trip',
+        body: 'Going to Utah'
+    },
+    {
+        title: 'Productivity',
+        body: 'Exercise. Eating better'
+    },
+    {
+        title: 'Coding',
+        body: 'Practice more frequently'
+    }
+];
 
 // removes last item in the array, you can save this to a variable if needed like such
 // let removedNote = notes.pop();
@@ -18,9 +31,27 @@ const notes = ['Note 1', 'Note 2', 'Note 3'];
 // notes.splice(1,1)//deletes one item at index 1
 // notes.splice(1,0,'New Note 2')//will add Note 2 back where we just deleted it
 //checks the length of the array
-notes.forEach()
+// notes.forEach(function (item, index){
+//     console.log(item, index)
+// })
 
-console.log(notes.length);
-console.log(notes);
+const findNote =  function (notes, noteTitle) {
+    const index =  notes.findIndex(function (note, index){
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+    return notes[index]
+}
+
+const note = findNote(notes, 'My next trip')
+console.log(note)
+
+// console.log(notes.length);
+// console.log(notes);
+
+// //This method on indexes, findIndex will run on each item in the array fo objects and once it find a match, it will return the index in value which were trying to match
+// const index = notes.findIndex(function (note, index) {
+//     return note.title === 'Habbits to work on'
+// })
+// console.log(index)
 
 
--- arrays/todo.js
+++ arrays/todo.js
@@ -4,7 +4,14 @@ todos.splice(2,1);
 todos.push('Wash dishes')
 todos.shift();
 
-console.log(`You have ${todos.length} todos!`);
-console.log(todos)
-// console.log(todos[todos.length -1], todos[todos.length -2])
+// console.log(`You have ${todos.length} todos!`);
+// console.log(todos)
+// // console.log(todos[todos.length -1], todos[todos.length -2])
 
+todos.forEach(function (todo, index) {
+    console.log(`${index + 1}. ${todo}`)
+})
+
+for(i = 0; i < todos.length; i++){
+    console.log(`${i + 1}. ${todos[i]}`)
+}
